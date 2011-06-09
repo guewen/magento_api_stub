3d4cabf720cc06af7bd6563efac0d2c7277bf1a5
Guewen Baconnier <guewen@gmail.com>
2011-06-09 22:05:03 +0200

    initial commit

---
+++ lib/fake_magento.rb
@@ -0,0 +1,34 @@
+#    FakeMagentoAPI
+#    Copyright (C) 2011 Guewen Baconnier
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+require 'logger'
+require "xmlrpc/server"
+
+s = XMLRPC::Server.new
+class MyHandler
+ def sum_and_difference(a, b)
+     { "sum" => a + b, "difference" => a - b }
+ end
+end
+s.add_introspection
+s.add_handler("sample", MyHandler.new)
+s.serve
+
+
+
+#require 'xmlrpc/client'
+#server = XMLRPC::Client.new("127.0.0.1", "/RPC2", 8080)
+#puts server.call("sample.sum_and_difference", 1, 2)
